014e1b8a0d810cc79bb16540cee3852e735789b8
ffff <3227198960@qq.com>
2026-09-22 17:41:40 +0800

    chore: add wrangler deploy config

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "XiaYe",
+	name: "ffff",
 
 	// 个人签名
-	bio: "Hello, I'm XiaYe.",
+	bio: "Hello.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
